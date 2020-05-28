bdde60fd9984d7c372f655a940b7093ebab97c07
Dark-Shaun <63874836+Dark-Shaun@users.noreply.github.com>
2020-05-29 02:35:27 +0530

    Update use.py

--- use.py
+++ use.py
@@ -2,7 +2,7 @@ import requests
 import json
 import pandas as pd
 def stock(ticker):
-    key = 'AUFSF50DIXVQWX6X'
+    key = ''
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&apikey='+key+'&datatype=csv'
     df = pd.read_csv(url)         #for csv read
     return df
